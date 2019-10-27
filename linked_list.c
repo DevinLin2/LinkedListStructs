dabdac44adf47584d69ed6dcf496a92661781184
Devin Lin <dlin01@stuy.edu>
2019-10-26 22:27:14 -0400

    insert front functions

--- linked_list.c
+++ linked_list.c
@@ -7,13 +7,25 @@ Should take a pointer to a node struct and print out all of the data in the list
 */
 void print_list(struct node * n) {
   printf("[ %d, ", n.number);
-  while (*n.next != NULL) {
-    n = *n.next;
-    if (*n.next == NULL) {
-      printf("%d ", *n.number);
+  while (n -> next != NULL) {
+    n = n -> next;
+    if (n -> next == NULL) {
+      printf("%d ", n -> number);
     } else {
-      printf("%d, ", *n.number);
+      printf("%d, ", n -> number);
     }
   }
   printf("]");
+}
+
+/*
+Should take a pointer to the existing list and the data to be added, create a new node and put it at the beginning of the list.
+The second argument should match whatever data you contain in your nodes.
+Returns a pointer to the beginning of the list.
+*/
+struct node * insert_front(struct node * n, int data) {
+  struct node * front = malloc(sizeof(struct node));
+  front -> number = data;
+  front -> next = n;
+  return front;
 }
